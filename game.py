da3ce58296271a4e537b462d5f7da838a2b9c6e0
Lily Lemmer <lily.lemmer@ave81.com>
2016-05-02 16:14:21 -0500

    add keys, inventory

--- game.py
+++ game.py
@@ -1,31 +1,137 @@
+# TODO: roomscript
 import glob
 import urwid
 
 
+class Key(object):
+    """A key to any door usable once!
+
+    """
+
+    def __init__(self):
+        self.name = "key"  # may be able to set in future
+
+
+class Door(object):
+
+    def __init__(self, link_id, needs_key=False):
+        self.link_id = link_id
+        self.needs_key = needs_key
+
+    @property
+    def name(self):
+
+        return self.link_id
+
+
+class Inventory(list):
+
+    def iter_items_matching_name(self, name):
+        """Don't use this, use the wrapper!"""
+
+        for i, item in enumerate(self):
+
+            if item.name.lower() == name.lower():
+                yield i, item
+
+    def iter_matched_items(self, func):
+
+        def wrapped_func(name):
+
+            for i, item in self.iter_items_matching_name(name):
+                yield func(i, item)
+
+        return wrapped_func
+
+    def get_first_item_matching_name(self, name):
+
+        for i, item in self.iter_items_matching_name(name):
+            return item
+
+        return None
+
+    def has_item_of_name(self, name):
+
+        for i, item in self.iter_items_matching_name(name):
+            return True
+
+        return False
+
+    def remove_item_of_name(self, name):
+
+        for i, item in self.iter_items_matching_name(name):
+            del self[i]
+            return None
+
+
+class Player(object):
+
+    def __init__(self, inventory=None):
+        self.inventory = inventory or Inventory()
+
+
 class Room(object):
 
-    def __init__(self, link_id, title, exits, description):
+    def __init__(self, link_id, title, exits, description, inventory=None):
         self.link_id = link_id
         self.title = title
         self.exits = exits
         self.description = description
+        self.inventory = inventory or Inventory()
+
+    def pickup_item(self, player, item_name):
+
+        for inventory_index, item in enumerate(self.inventory):
+
+            if item.name.lower() == item_name.lower():
+                player.inventory.append(item)
+                del self.inventory[inventory_index]
+                return item
 
     @staticmethod
     def validate_and_clean(expected_row_name, unsafe_row):
         expected_row_name_with_syntax = expected_row_name.upper() + ": "
-        assert unsafe_row.startswith(expected_row_name_with_syntax)
+        assert unsafe_row.startswith(expected_row_name_with_syntax), (expected_row_name, unsafe_row)
         row_without_name = unsafe_row.replace(expected_row_name_with_syntax, " ", 1) 
         return row_without_name.strip()
     
     @classmethod
     def from_string(cls, room_string):
+        """RoomScript"""
+
         room_file_contents = room_string.split('\n')
         link_id = cls.validate_and_clean("link_id", room_file_contents[0]).lower()
         title = cls.validate_and_clean("title", room_file_contents[1])
-        exits = cls.validate_and_clean("exits", room_file_contents[2]).split(",")
-        description = '\n'.join(room_file_contents[3:]).strip()
 
-        return cls(link_id, title, exits, description)
+        # Exits are a little bit more complicated...
+        exit_rules = cls.validate_and_clean("exits", room_file_contents[2]).split(",")
+        exits = []
+
+        for rule in exit_rules:
+
+            if "[!LOCKED!]" in rule:
+                door_is_locked = True
+                link_name = rule.replace("[!LOCKED!]", "")
+            else:
+                door_is_locked = False
+                link_name = rule
+
+            door = Door(link_id=link_name, needs_key=door_is_locked)
+            exits.append(door)
+
+        # the room's items/inventory
+        inventory_rules = cls.validate_and_clean("inventory", room_file_contents[3]).split(",")
+        inventory = Inventory()
+
+        for rule in inventory_rules:
+
+            if rule:
+                item_to_add = {"key": Key}[rule]
+                inventory.append(item_to_add())
+
+        # finally the description and make room
+        description = '\n'.join(room_file_contents[4:]).strip()
+        return cls(link_id, title, exits, description, inventory=inventory)
 
 
 class Adventure(object):
@@ -33,6 +139,7 @@ class Adventure(object):
 
     def __init__(self, rooms):
         self.rooms = rooms
+        self.player = Player()
         self.current_room = self.rooms["first_room"]
 
     @classmethod
@@ -57,12 +164,13 @@ class RoomUI(object):
     OVERLAY_HEIGHT_IN_PERCENT = 60
 
     def __init__(self, adventure):
-
         self.adventure = adventure
         self.main = None
 
     def menu(self):
         """Menu from current room.
+
+        Needs to add thing for picking up items.
 
         """
 
@@ -73,9 +181,14 @@ class RoomUI(object):
                 urwid.Text(description),
                 urwid.Divider()]
 
-        for c in self.adventure.current_room.exits:
-            button = urwid.Button(c)
-            urwid.connect_signal(button, 'click', self.item_chosen, c)
+        for c in self.adventure.current_room.exits + self.adventure.current_room.inventory:
+            button_name = c.name
+
+            if hasattr(c, "needs_key") and c.needs_key:
+                button_name += " [LOCKED]"
+
+            button = urwid.Button(button_name)
+            urwid.connect_signal(button, 'click', self.item_chosen, c.name)
             body.append(urwid.AttrMap(button, None, focus_map='reversed'))
 
         button = urwid.Button("QUIT")
@@ -88,8 +201,13 @@ class RoomUI(object):
         raise urwid.ExitMainLoop()
 
     def make_room(self):
+        """GUI"""
         listbox_menu = self.menu()
         self.main.original_widget = listbox_menu
+
+    def list_inventory(self):
+        """asdf"""
+        self.main.original_widget = new_thing
 
     def item_chosen(self, button, choice):
         """Can use this to create menu again.
@@ -99,8 +217,27 @@ class RoomUI(object):
         # change the current room to choice
         # TODO: this should probably be a method of adventure,
         # such as getitem
-        self.adventure.set_room_by_link_id(choice)
+        current_room = self.adventure.current_room
+
+        for door in current_room.exits:
+
+            if door.name.lower() == choice:
+
+                if door.needs_key:
+                    
+                    if self.adventure.player.inventory.has_item_of_name('key'):
+                        door.needs_key = False
+                        self.adventure.player.inventory.remove_item_of_name('key')
+                    else:
+                        continue
+
+                self.adventure.set_room_by_link_id(choice)
+                self.make_room()
+                return None
+
+        current_room.pickup_item(self.adventure.player, choice)
         self.make_room()
+        return None
 
     def run(self):
         listbox_menu = self.menu()
@@ -112,6 +249,7 @@ class RoomUI(object):
                             valign='middle', height=overlay_height,
                             min_width=20, min_height=9)
         urwid.MainLoop(top, palette=[('reversed', 'standout', '')]).run()
+
 
 # sorting out urwid gui mess
 adventure = Adventure.from_directory("rooms")
